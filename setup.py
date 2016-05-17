33a31b611eb84c963f6d07e97d012933dee9e093
Bennett Landman <landmaba@plausibyhorizon.masi.local>
2016-05-17 11:07:03 -0500

    PiPy setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyPheWAS',
+  packages = ['pyPheWAS'], # this must be the same as the name above
+  version = '0.1',
+  description = 'MASI Lab Port of PheWAS into Python',
+  author = 'MASI Lab',
+  author_email = 'bennett.landman@vanderbilt.edu',
+  url = 'https://github.com/bennettlandman/pyPheWAS', # use the URL to the github repo
+  download_url = 'https://github.com/bennettlandman/pyPheWAS/tarball/0.1', # I'll explain this in a second
+  keywords = ['PheWAS', 'ICD-9', 'EMR'], # arbitrary keywords
+  classifiers = [],
+)
