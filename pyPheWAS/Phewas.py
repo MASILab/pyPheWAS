15b518141f7614d21a25267e5819634d74bcf20f
kunalnabar <kunalnabar@gmail.com>
2016-06-07 12:04:43 -0500

    Changed Phewas object

---
+++ pyPheWAS/Phewas.py
@@ -0,0 +1,25 @@
+"""
+
+
+"""
+class Phewas:
+	
+	"""
+	This is the object that is used to run Phewas.
+
+	Checks the validity of arguments such as:
+	* Valid Phewas file structure
+	* Valid values for the arguments passed
+
+	Allows for option such as:
+	* Age-matching and control
+	* Matching and control by other criteria
+	"""
+	
+	def __init__(self):
+		"""
+		
+		
+		"""
+		pass
+	
