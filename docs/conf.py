44fe29dae95eda2e0738b577a5e8f5d3452d3efa
kunalnabar <kunalnabar@gmail.com>
2016-06-08 12:12:42 -0500

    adding back get_codes docstring

--- docs/conf.py
+++ docs/conf.py
@@ -21,12 +21,6 @@ import os
 # sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('..'))
 
-"""import mock
- 
-MOCK_MODULES = [ 'matplotlib', 'matplotlib.pyplot', 'pandas', 'pandas.codre']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()"""
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
