1c99222f2aa5304f23777edf348c9d24671db5d0
Bennett Landman <landmaba@plausibyhorizon.masi.local>
2016-06-08 11:21:55 -0500

    added mock numpy

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,11 @@ import os
 # sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('..'))
 
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
