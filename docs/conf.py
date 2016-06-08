3cb85939dec0565c2c46d19cb60994c75d55708b
Bennett Landman <landmaba@plausibyhorizon.masi.local>
2016-06-08 11:40:29 -0500

    more mock stupidity + 5

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import mock
  
-MOCK_MODULES = [ 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'stats', 'scipy.stats', 'pandas']
+MOCK_MODULES = [ 'matplotlib', 'matplotlib.pyplot', 'pandas']
 for mod_name in MOCK_MODULES:
  sys.modules[mod_name] = mock.Mock()
 
