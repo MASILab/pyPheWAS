8ab423e4773066efa6eb53125f41481afcb08fe3
Bennett Landman <landmaba@plausibyhorizon.masi.local>
2016-06-08 11:24:11 -0500

    stupid indent

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+ sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
