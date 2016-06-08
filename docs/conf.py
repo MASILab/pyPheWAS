56c8c34e254a3381405f41cebe95bb5e7e989004
kunalnabar <kunalnabar@gmail.com>
2016-06-08 12:07:34 -0500

    still trying to fix readthedocs, added requirements file

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,11 @@ import os
 # sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('..'))
 
-import mock
+"""import mock
  
-MOCK_MODULES = [ 'matplotlib', 'matplotlib.pyplot', 'pandas']
+MOCK_MODULES = [ 'matplotlib', 'matplotlib.pyplot', 'pandas', 'pandas.codre']
 for mod_name in MOCK_MODULES:
- sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()"""
 
 # -- General configuration ------------------------------------------------
 
