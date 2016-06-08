8a203249b09e053d8dec4a0531425e24bb105acc
Masi <masi@masis-imac.masi.local>
2016-06-08 10:56:35 -0500

    trying to fix the readthedocs bug

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,10 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('..'))
+sep = os.sep
+path = os.path.abspath(os.curdir)
+path = path[:path.index('pyPheWAS')] + os.sep + 'pyPheWAS/'
+sys.path.insert(0, os.path.abspath(path))
 
 # -- General configuration ------------------------------------------------
 
