d644675b8fce31db897abdbdc7aa455d29d06f5f
gumchewer <shikha.chaganti@vanderbilt.edu>
2018-04-20 09:44:19 -0500

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pyPheWAS',
   packages = ['pyPheWAS'], # this must be the same as the name above
-  version = '0.2.4',
+  version = '0.2.5',
   description = 'MASI Lab Port of PheWAS into Python',
   author = 'MASI Lab',
   author_email = 'bennett.landman@vanderbilt.edu',
