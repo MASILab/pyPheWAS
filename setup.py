e7c5f01460239555de091a405ab90f6bc5023ae5
clineci <cailey.i.cline@vanderbilt.edu>
2019-02-11 15:12:51 -0600

    updated version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyPheWAS',
   packages = ['pyPheWAS'], # this must be the same as the name above
-  version = '0.2.15',
+  version = '1.0.0',
   description = 'MASI Lab Port of PheWAS into Python',
   author = 'MASI Lab',
   author_email = 'bennett.landman@vanderbilt.edu',
-  url = 'https://github.com/bennettlandman/pyPheWAS', # use the URL to the github repo
-  download_url = 'https://github.com/bennettlandman/pyPheWAS/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/MASILab/pyPheWAS', # use the URL to the github repo
+  download_url = 'https://github.com/MASILab/pyPheWAS/tarball/0.1', # I'll explain this in a second
   keywords = ['PheWAS', 'ICD-9', 'EMR'], # arbitrary keywords
   classifiers = [],
   install_requires=['numpy',
