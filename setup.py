2e551f2c93ac066f0ecf8ffd4d1ab04b7bbcbae1
clineci <cailey.i.cline@vanderbilt.edu>
2019-03-14 08:34:14 -0500

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pyPheWAS',
   packages = ['pyPheWAS'], # this must be the same as the name above
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'MASI Lab Port of PheWAS into Python',
   author = 'MASI Lab',
   author_email = 'bennett.landman@vanderbilt.edu',
