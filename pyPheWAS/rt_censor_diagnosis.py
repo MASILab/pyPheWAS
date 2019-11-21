a7eb12249090ae30fd2cd038a03bbeb79e82122a
clineci <cailey.i.cline@vanderbilt.edu>
2019-11-21 16:23:09 -0600

    removed secondary censoring option because it was confusing via the docs

--- pyPheWAS/rt_censor_diagnosis.py
+++ pyPheWAS/rt_censor_diagnosis.py
@@ -1,32 +1,33 @@
-def censor_diagnosis(genotype_file,phenotype_file,final_pfile, final_gfile,field ='na',start_time=float('nan'),end_time=float('nan')):
+def censor_diagnosis(genotype_file,phenotype_file,final_pfile, final_gfile,field,start_time=float('nan'),end_time=float('nan')):
         import pandas as pd
         import numpy as np
         genotypes = pd.read_csv(genotype_file)
         phenotypes = pd.read_csv(phenotype_file)
         mg=pd.merge(phenotypes,genotypes,on='id')
-        if np.isnan(start_time) and np.isnan(end_time):
-                print("Choose appropriate time period")
-        if field=='na':
-                if np.isfinite(start_time) and np.isnan(end_time):
-                        final = mg[mg['AgeAtICD']>=start_time]
-                elif np.isnan(start_time) and np.isfinite(end_time):
-                        final = mg[mg['AgeAtICD']<=end_time]
-                else:
-                        final = mg[(mg['AgeAtICD']>=start_time)&(mg['AgeAtICD']<=end_time)]
+        assert field in mg, 'Specified field (%s) does not exist in phenotype or genotype field' %field
 
+        if np.isfinite(start_time) and np.isnan(end_time):
+                final = mg[mg[field]>=start_time]
+        elif np.isnan(start_time) and np.isfinite(end_time):
+                final = mg[mg[field]<=end_time]
         else:
-                mg['diff']=mg[field]-mg['AgeAtICD']
-                if np.isfinite(start_time) and np.isnan(end_time):
-                        final = mg[(mg['diff']>=start_time)|(np.isnan(mg['diff']))]
-                elif np.isnan(start_time) and np.isfinite(end_time):
-                        final = mg[(mg['diff']<=end_time)|(np.isnan(mg['diff']))]
-                else:
-                        final = mg[(mg['diff']>=start_time)&(mg['diff']<=end_time)|(np.isnan(mg['diff']))]
-        final[['id','icd9','AgeAtICD']].to_csv(final_pfile)
+                final = mg[(mg[field]>=start_time)&(mg[field]<=end_time)]
+
+        # for censoring with respect to the interval between AgeAtICD and a specified field (i.e. AgeAtDX or age at a scan time)
+        # else:
+        #         mg['diff']=mg[field]-mg['AgeAtICD']
+        #         if np.isfinite(start_time) and np.isnan(end_time):
+        #                 final = mg[(mg['diff']>=start_time)|(np.isnan(mg['diff']))]
+        #         elif np.isnan(start_time) and np.isfinite(end_time):
+        #                 final = mg[(mg['diff']<=end_time)|(np.isnan(mg['diff']))]
+        #         else:
+        #                 final = mg[(mg['diff']>=start_time)&(mg['diff']<=end_time)|(np.isnan(mg['diff']))]
+
+        final.to_csv(final_pfile,columns=phenotypes.columns,index=False)
+        print("New phenotype file in %s" % final_pfile)
         final_gp = final.drop_duplicates('id')
-        del final_gp['icd9']
-        del final_gp['AgeAtICD']
-        final_gp.to_csv(final_gfile)
+        final_gp.to_csv(final_gfile,columns=genotypes.columns,index=False)
+        print("New group file in %s" % final_gfile)
         
 
 
