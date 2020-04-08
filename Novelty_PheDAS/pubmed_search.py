76a769ccdf0f96144a7ff76046e4b7446225dca7
clineci <cailey.i.cline@vanderbilt.edu>
2020-04-08 01:33:58 -0500

    limit to english

--- Novelty_PheDAS/pubmed_search.py
+++ Novelty_PheDAS/pubmed_search.py
@@ -71,6 +71,10 @@ def main():
     ### Get Args ###
     args = parse_args()
 
+    outdir = args.outdir
+    if not osp.exists(outdir):
+        os.mkdir(outdir)
+
     print('Reading ICD10 file...')
     icd10 = pd.read_csv(args.icd10)
     icd10.dropna(subset=['PheCode'],inplace=True)
@@ -81,9 +85,7 @@ def main():
     # TODO: check reading with read_csv instead
     umls = pd.read_table(args.umls,sep='|',header=None,names=umls_cols,low_memory=False)
     umls.drop(columns='other',inplace=True)
-    outdir = args.outdir
-    if ~ osp.exists(outdir):
-        os.mkdir(outdir)
+    umls = umls[umls['LAT'] == 'ENG'] # limit search to english terms
 
     pubmed_list = {}
     missed = []
@@ -111,53 +113,50 @@ def main():
                 cui = pd.merge(umls_icd9, phe_icd9, left_on='CODE', right_on='ICD9')
             all_cui_str = pd.merge(umls, cui[['CUI']], on='CUI')
 
-            ss = '('
-            first = True
             for ix, data in tqdm(all_cui_str.iterrows(),total=all_cui_str.shape[0]):
                 # build search string from CUI str
-                if not first: ss = ss+ ' OR ('
-                else: first = False
+                ss='('
                 for term in data['STR'].split():
                     if len(term)>0:
                         ss=ss+term.strip()+ '+'
                 ss=ss[:-1]+ ')'
 
-            # search Titles & Abstracts
-            ss_r = search(ss, 0, 'TIAB')
+                # search Titles & Abstracts
+                ss_r = search(ss, 0, 'TIAB')
 
-            # parse results
-            gx1 = ss_r['Count']
-            if int(gx1)>1000000:
-                # dump super big results to weird.pickle because this is weird
-                weird.append(ss)
-                pickle_wd = open(osp.join(outdir,"weird.pickle"), "wb")
-                pickle.dump(weird, pickle_wd)
-                pickle_wd.close()
-                continue
-            else:
-                uids = uids.union(ss_r['IdList'])
-                # only 10,000 results returned at a time, so keep going to get all of the uids
-                for n in range(10000, int(gx1), 10000):
-                   uids = uids.union(search(ss, n, 'TIAB')['IdList'])
+                # parse results
+                gx1 = ss_r['Count']
+                if int(gx1)>1000000:
+                    # dump super big results to weird.pickle because this is weird
+                    weird.append(ss)
+                    pickle_wd = open(osp.join(outdir,"weird.pickle"), "wb")
+                    pickle.dump(weird, pickle_wd)
+                    pickle_wd.close()
+                    continue
+                else:
+                    uids = uids.union(ss_r['IdList'])
+                    # only 10,000 results returned at a time, so keep going to get all of the uids
+                    for n in range(10000, int(gx1), 10000):
+                       uids = uids.union(search(ss, n, 'TIAB')['IdList'])
 
 
-            # repeat search, looking in Keywords field
-            ss_r = search(ss, 0, 'MESH')
+                # repeat search, looking in Keywords field
+                ss_r = search(ss, 0, 'MESH')
 
-            # parse results
-            gx1 = ss_r['Count']
-            if int(gx1)>1000000:
-                # dump super big results to weird.pickle because this is weird
-                weird.append(ss)
-                pickle_wd = open(osp.join(outdir,"weird.pickle"), "wb")
-                pickle.dump(weird, pickle_wd)
-                pickle_wd.close()
-                continue
-            else:
-                uids = uids.union(ss_r['IdList'])
-                # only 10,000 results returned at a time, so keep going to get all of the uids
-                for n in range(10000, int(gx1), 10000):
-                    uids = uids.union(search(ss, n, 'MESH')['IdList'])
+                # parse results
+                gx1 = ss_r['Count']
+                if int(gx1)>1000000:
+                    # dump super big results to weird.pickle because this is weird
+                    weird.append(ss)
+                    pickle_wd = open(osp.join(outdir,"weird.pickle"), "wb")
+                    pickle.dump(weird, pickle_wd)
+                    pickle_wd.close()
+                    continue
+                else:
+                    uids = uids.union(ss_r['IdList'])
+                    # only 10,000 results returned at a time, so keep going to get all of the uids
+                    for n in range(10000, int(gx1), 10000):
+                        uids = uids.union(search(ss, n, 'MESH')['IdList'])
 
 
             # Repeat search process, but search the PheCode string
