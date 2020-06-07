b4e8f5d69ab842964fd0788ed50fb827407363b1
clineci <cailey.i.cline@vanderbilt.edu>
2020-06-07 14:20:02 -0500

    merge

--- Novelty_PheDAS/pubmed_search.py
+++ Novelty_PheDAS/pubmed_search.py
@@ -68,71 +68,143 @@ def main():
     weird_phw = []
 
     print('Making subset tables... ')
-    phecode_list = icd9.drop_duplicates(subset=['PheCode']).reset_index(drop=True).sort_values(by=['PheCode']).copy()
+    phecode_list = icd9.drop_duplicates(subset=['PheCode']).sort_values(by=['PheCode']).reset_index(drop=True).copy()
     umls_icd10 = umls[umls['SAB'] == 'ICD10'].copy()
     umls_icd9 = umls[umls['SAB'] == 'ICD9CM'].copy()
     
-    e_file = open(osp.join(outdir,'errors.csv'),'w+')
-    e_file.write('PheCode,SearchType,SearchString,ErrorMsg\n')
+    with open(osp.join(outdir,'errors.csv'),'w+') as e_file:
+        e_file.write('PheCode,SearchType,SearchString,ErrorMsg\n')
 
-    for ix, data in tqdm(phecode_list.iterrows(),total=phecode_list.shape[0]):
-        uids = set() # unique identifiers of pubmed articles
-        phe = data['PheCode']
-        # print(phe)
-
-        # Get all icd codes the map to current PheCode
-        phe_icd10 = icd10[icd10['PheCode'] == phe]
-        phe_icd9 = icd9[icd9['PheCode'] == phe]
-        # Get all CUI codes the map those ICD codes
-        if phe_icd10.shape[0] > 0:
-            cui_icd10 = pd.merge(umls_icd10, phe_icd10, left_on='CODE', right_on='ICD10')
-            cui_icd9 = pd.merge(umls_icd9, phe_icd9, left_on='CODE', right_on='ICD9')
-            cui = cui_icd10.append(cui_icd9,sort=False)
-        else:
-            # print('Only ICD9')
-            cui = pd.merge(umls_icd9, phe_icd9, left_on='CODE', right_on='ICD9')
-        all_cui_str = pd.merge(umls, cui[['CUI']], on='CUI').drop_duplicates(subset=['CUI','STR'])
-
-        num_cui = all_cui_str.shape[0]
-        for k, group in tqdm(all_cui_str.groupby(np.arange(num_cui)//10),total=len(np.unique(np.arange(num_cui)//10))):
-            # build search string from CUI strings
-            ss = '('
-            first = True
-            for ix2, data2 in group.iterrows():
-                if not first: ss = ss + 'OR('
-                else: first = False
-                for term in data2['STR'].split():
-                    if len(term)>0:
-                        ss=ss+term.strip()+ ' AND '
-                ss = ss[:-5]+ ')'
-                
+        for ix, data in tqdm(phecode_list.iterrows(),total=phecode_list.shape[0]):
+            uids = set() # unique identifiers of pubmed articles
+            phe = data['PheCode']
+            # print(phe)
+    
+            # Get all icd codes the map to current PheCode
+            phe_icd10 = icd10[icd10['PheCode'] == phe]
+            phe_icd9 = icd9[icd9['PheCode'] == phe]
+            # Get all CUI codes the map those ICD codes
+            if phe_icd10.shape[0] > 0:
+                cui_icd10 = pd.merge(umls_icd10, phe_icd10, left_on='CODE', right_on='ICD10')
+                cui_icd9 = pd.merge(umls_icd9, phe_icd9, left_on='CODE', right_on='ICD9')
+                cui = cui_icd10.append(cui_icd9,sort=False)
+            else:
+                # print('Only ICD9')
+                cui = pd.merge(umls_icd9, phe_icd9, left_on='CODE', right_on='ICD9')
+            all_cui_str = pd.merge(umls, cui[['CUI']], on='CUI').drop_duplicates(subset=['CUI','STR'])
+    
+            num_cui = all_cui_str.shape[0]
+            for k, group in tqdm(all_cui_str.groupby(np.arange(num_cui)//10),total=len(np.unique(np.arange(num_cui)//10))):
+                # build search string from CUI strings
+                ss = '('
+                first = True
+                for ix2, data2 in group.iterrows():
+                    if not first: ss = ss + 'OR('
+                    else: first = False
+                    for term in data2['STR'].split():
+                        if len(term)>0:
+                            ss=ss+term.strip()+ ' AND '
+                    ss = ss[:-5]+ ')'
+                    
+                try:
+                    # search Titles & Abstracts
+                    ss_r = search(ss, 0, 'TIAB')
+    
+                    # parse results
+                    gx1 = ss_r['Count']
+                    # print(gx1)
+                    if int(gx1)>1000000:
+                        # dump super big results to weird.pickle because this is weird
+                        e_file.write('%s,%s,%s,%s\n' % (str(phe), 'TIAB', ss, 'Weird'))
+                        weird_phe.append(phe)
+                        weird.append(ss)
+                        pickle_wd = open(osp.join(outdir,"weird.pickle"), "wb")
+                        pickle.dump(weird_phe, pickle_wd)
+                        pickle.dump(weird, pickle_wd)
+                        pickle_wd.close()
+                        continue
+                    else:
+                        uids = uids.union(ss_r['IdList'])
+                        # only 10,000 results returned at a time, so keep going to get all of the uids
+                        for n in range(10000, int(gx1), 10000):
+                           uids = uids.union(search(ss, n, 'TIAB')['IdList'])
+                           
+                except Exception as e:
+                    e_file.write('%s,%s,%s,%s\n' %(str(phe),'TIAB',ss,e.args[0]))
+                    missed_phe.append(phe)
+                    missed.append(ss)
+                    pickle_ms = open(osp.join(outdir, "missed.pickle"), "wb")
+                    pickle.dump(missed_phe, pickle_ms)
+                    pickle.dump(missed, pickle_ms)
+                    pickle_ms.close()
+                    time.sleep(30)
+                    
+                try:
+                    # repeat search, looking in Keywords field
+                    ss_r = search(ss, 0, 'MESH')
+    
+                    # parse results
+                    gx1 = ss_r['Count']
+                    # print(gx1)
+                    if int(gx1)>1000000:
+                        # dump super big results to weird.pickle because this is weird
+                        e_file.write('%s,%s,%s,%s\n' % (str(phe), 'MESH', ss, 'Weird'))
+                        weird_phe.append(phe)
+                        weird.append(ss)
+                        pickle_wd = open(osp.join(outdir, "weird.pickle"), "wb")
+                        pickle.dump(weird_phe, pickle_wd)
+                        pickle.dump(weird, pickle_wd)
+                        pickle_wd.close()
+                        continue
+                    else:
+                        uids = uids.union(ss_r['IdList'])
+                        # only 10,000 results returned at a time, so keep going to get all of the uids
+                        for n in range(10000, int(gx1), 10000):
+                            uids = uids.union(search(ss, n, 'MESH')['IdList'])
+                            
+                except Exception as e:
+                    e_file.write('%s,%s,%s,%s\n' %(str(phe),'MESH',ss,e.args[0]))
+                    missed_phe.append(phe)
+                    missed.append(phe)
+                    pickle_ms = open(osp.join(outdir, "missed.pickle"), "wb")
+                    pickle.dump(missed_phe, pickle_ms)
+                    pickle.dump(missed, pickle_ms)
+                    pickle_ms.close()
+                    time.sleep(30)
+                    
             try:
+                # Repeat search process, but search the PheCode string
+                ss = '('
+                for term in data['Phenotype'].split():
+                    # build search string
+                    if len(term) > 0:
+                        ss = ss + term.strip() + ' '
+                ss = ss + ')'
+    
                 # search Titles & Abstracts
                 ss_r = search(ss, 0, 'TIAB')
-
                 # parse results
                 gx1 = ss_r['Count']
                 # print(gx1)
-                if int(gx1)>1000000:
+                if int(gx1) > 1000000:
                     # dump super big results to weird.pickle because this is weird
                     e_file.write('%s,%s,%s,%s\n' % (str(phe), 'TIAB', ss, 'Weird'))
                     weird_phe.append(phe)
                     weird.append(ss)
-                    pickle_wd = open(osp.join(outdir,"weird.pickle"), "wb")
+                    pickle_wd = open(osp.join(outdir, "weird.pickle"), "wb")
                     pickle.dump(weird_phe, pickle_wd)
                     pickle.dump(weird, pickle_wd)
                     pickle_wd.close()
-                    continue
                 else:
                     uids = uids.union(ss_r['IdList'])
                     # only 10,000 results returned at a time, so keep going to get all of the uids
                     for n in range(10000, int(gx1), 10000):
-                       uids = uids.union(search(ss, n, 'TIAB')['IdList'])
-                       
+                        uids = uids.union(search(ss, n, 'TIAB')['IdList'])
+                        
             except Exception as e:
                 e_file.write('%s,%s,%s,%s\n' %(str(phe),'TIAB',ss,e.args[0]))
                 missed_phe.append(phe)
-                missed.append(ss)
+                missed.append(phe)
                 pickle_ms = open(osp.join(outdir, "missed.pickle"), "wb")
                 pickle.dump(missed_phe, pickle_ms)
                 pickle.dump(missed, pickle_ms)
@@ -142,11 +214,10 @@ def main():
             try:
                 # repeat search, looking in Keywords field
                 ss_r = search(ss, 0, 'MESH')
-
                 # parse results
                 gx1 = ss_r['Count']
                 # print(gx1)
-                if int(gx1)>1000000:
+                if int(gx1) > 1000000:
                     # dump super big results to weird.pickle because this is weird
                     e_file.write('%s,%s,%s,%s\n' % (str(phe), 'MESH', ss, 'Weird'))
                     weird_phe.append(phe)
@@ -155,7 +226,6 @@ def main():
                     pickle.dump(weird_phe, pickle_wd)
                     pickle.dump(weird, pickle_wd)
                     pickle_wd.close()
-                    continue
                 else:
                     uids = uids.union(ss_r['IdList'])
                     # only 10,000 results returned at a time, so keep going to get all of the uids
@@ -171,90 +241,20 @@ def main():
                 pickle.dump(missed, pickle_ms)
                 pickle_ms.close()
                 time.sleep(30)
-                
-        try:
-            # Repeat search process, but search the PheCode string
-            ss = '('
-            for term in data['Phenotype'].split():
-                # build search string
-                if len(term) > 0:
-                    ss = ss + term.strip() + ' '
-            ss = ss + ')'
-
-            # search Titles & Abstracts
-            ss_r = search(ss, 0, 'TIAB')
-            # parse results
-            gx1 = ss_r['Count']
-            # print(gx1)
-            if int(gx1) > 1000000:
-                # dump super big results to weird.pickle because this is weird
-                e_file.write('%s,%s,%s,%s\n' % (str(phe), 'TIAB', ss, 'Weird'))
-                weird_phe.append(phe)
-                weird.append(ss)
-                pickle_wd = open(osp.join(outdir, "weird.pickle"), "wb")
-                pickle.dump(weird_phe, pickle_wd)
-                pickle.dump(weird, pickle_wd)
-                pickle_wd.close()
-            else:
-                uids = uids.union(ss_r['IdList'])
-                # only 10,000 results returned at a time, so keep going to get all of the uids
-                for n in range(10000, int(gx1), 10000):
-                    uids = uids.union(search(ss, n, 'TIAB')['IdList'])
-                    
-        except Exception as e:
-            e_file.write('%s,%s,%s,%s\n' %(str(phe),'TIAB',ss,e.args[0]))
-            missed_phe.append(phe)
-            missed.append(phe)
-            pickle_ms = open(osp.join(outdir, "missed.pickle"), "wb")
-            pickle.dump(missed_phe, pickle_ms)
-            pickle.dump(missed, pickle_ms)
-            pickle_ms.close()
-            time.sleep(30)
-                
-        try:
-            # repeat search, looking in Keywords field
-            ss_r = search(ss, 0, 'MESH')
-            # parse results
-            gx1 = ss_r['Count']
-            # print(gx1)
-            if int(gx1) > 1000000:
-                # dump super big results to weird.pickle because this is weird
-                e_file.write('%s,%s,%s,%s\n' % (str(phe), 'MESH', ss, 'Weird'))
-                weird_phe.append(phe)
-                weird.append(ss)
-                pickle_wd = open(osp.join(outdir, "weird.pickle"), "wb")
-                pickle.dump(weird_phe, pickle_wd)
-                pickle.dump(weird, pickle_wd)
-                pickle_wd.close()
-            else:
-                uids = uids.union(ss_r['IdList'])
-                # only 10,000 results returned at a time, so keep going to get all of the uids
-                for n in range(10000, int(gx1), 10000):
-                    uids = uids.union(search(ss, n, 'MESH')['IdList'])
-                    
-        except Exception as e:
-            e_file.write('%s,%s,%s,%s\n' %(str(phe),'MESH',ss,e.args[0]))
-            missed_phe.append(phe)
-            missed.append(phe)
-            pickle_ms = open(osp.join(outdir, "missed.pickle"), "wb")
-            pickle.dump(missed_phe, pickle_ms)
-            pickle.dump(missed, pickle_ms)
-            pickle_ms.close()
-            time.sleep(30)
-
-
-        pubmed_list[phe] = list(uids)
-        # print(pubmed_list.items())
-        # print(len(uids))
-
-        if np.mod(ix, 10) == 0:
-            uid_df = pd.DataFrame(pubmed_list.items(),columns=['PheCode','IdsList'])
-            uid_df.to_csv(osp.join(outdir,'phecode_pubmed_articles_'+str(ix)+'.csv'),index=False)
-            time.sleep(60)
-            pubmed_list = {}
-
-    uid_df = pd.DataFrame(pubmed_list.items(), columns=['PheCode', 'IdsList'])
-    uid_df.to_csv(osp.join(outdir, 'phecode_pubmed_articles_' + str(ix) + '.csv'), index=False)
+    
+    
+            pubmed_list[phe] = list(uids)
+            # print(pubmed_list.items())
+            # print(len(uids))
+    
+            if np.mod(ix, 10) == 0:
+                uid_df = pd.DataFrame(pubmed_list.items(),columns=['PheCode','IdsList'])
+                uid_df.to_csv(osp.join(outdir,'phecode_pubmed_articles_'+str(ix)+'.csv'),index=False)
+                time.sleep(60)
+                pubmed_list = {}
+    
+        uid_df = pd.DataFrame(pubmed_list.items(), columns=['PheCode', 'IdsList'])
+        uid_df.to_csv(osp.join(outdir, 'phecode_pubmed_articles_' + str(ix) + '.csv'), index=False)
 
     interval = time.time() - start
     hour = math.floor(interval / 3600.0)
