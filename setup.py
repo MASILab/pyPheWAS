6127a356c1cb98126598a674975780a37f7d8a56
clineci <cailey.i.kerley@vanderbilt.edu>
2023-09-29 11:00:58 -0500

    update to version 4.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'pyPheWAS',
   packages = ['pyPheWAS'], # this must be the same as the name above
-  version = '4.1.1',
+  version = '4.2.0',
   description = 'MASI Lab Port of PheWAS into Python',
   author = 'MASI Lab',
   author_email = 'bennett.landman@vanderbilt.edu',
@@ -21,7 +21,7 @@ setup(
                      'pathlib',
                      'biopython',
                      'flask',
-					 'flask_cors',
+                     'flask_cors',
                      ],
   package_data={
     '':['resources/*.csv', 'resources/*.txt','Explorer_GUI/*'],
